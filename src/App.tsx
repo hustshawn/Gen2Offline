38383a6e5195568b220604418f2178fac94972fc
Erik Hanchett <ehhanche@amazon.com>
2024-08-05 19:59:55 -0700

    Added persistence

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { useState, FormEvent } from "react";
 import { generateClient } from "aws-amplify/api";
 import { Schema } from "../amplify/data/resource";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const queryClient = useQueryClient();
@@ -19,17 +20,19 @@ function App() {
     queryKey: ["titles"],
     queryFn: async () => {
       const { data: allPosts } = await client.models.Post.list();
+
       return allPosts;
     },
   });
 
   // Mutations
   const mutation = useMutation({
+    mutationKey: ["addTitles"],
     mutationFn: async (title: string) => {
       const { data } = await client.models.Post.create({
         title,
       });
-      console.log("data", data);
+      console.log("data-mutate", data);
       return data;
     },
     onMutate: async (newTitles) => {
@@ -41,7 +44,7 @@ function App() {
           ["titles"],
           (old: Schema["Post"]["type"][]) => {
             console.log("old", [...old, newTitles]);
-            const record = { ...old[0], title: newTitles };
+            const record = { ...old[0], id: uuidv4(), title: newTitles };
             return [...old, record];
           }
         );
@@ -53,21 +56,36 @@ function App() {
     onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ["titles"] });
     },
-
-    onSuccess: () => {
-      // Invalidate and refetch
-      // queryClient.invalidateQueries({ queryKey: ["titles"] });
-    },
   });
 
   // Mutations
   const deleteMutation = useMutation({
+    mutationKey: ["deleteTitles"],
     mutationFn: async (id: string) => {
       const { data } = await client.models.Post.delete({
         id,
       });
-      console.log("data", data);
+      console.log("data-delete", data);
       return data;
+    },
+    onMutate: async (deletedTitle) => {
+      await queryClient.cancelQueries({ queryKey: ["titles"] });
+      const previousTitles = queryClient.getQueryData(["titles"]);
+
+      if (deletedTitle) {
+        queryClient.setQueryData(
+          ["titles"],
+          (old: Schema["Post"]["type"][]) => {
+            console.log("old", deletedTitle);
+            return old.filter((item) => item.id !== deletedTitle);
+          }
+        );
+      }
+
+      return { previousTitles };
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["titles"] });
     },
     onSuccess: () => {
       // Invalidate and refetch
@@ -77,18 +95,7 @@ function App() {
 
   const client = generateClient<Schema>();
   const { signOut } = useAuthenticator((context) => [context.user]);
-  // const [posts, setPosts] = useState<Schema["Post"]["type"][]>();
   const [post, setPost] = useState("");
-
-  // const getAllData = useCallback(async () => {
-  //   const { data: allPosts } = await client.models.Post.list();
-
-  //   setPosts(allPosts);
-  // }, [client.models.Post]);
-
-  // useEffect(() => {
-  //   getAllData();
-  // }, [getAllData]);
 
   async function onAddTitle(form: FormEvent<HTMLFormElement>) {
     form.preventDefault();
@@ -97,21 +104,11 @@ function App() {
 
     mutation.mutate(title);
 
-    // const { data } = await client.models.Post.create({
-    //   title,
-    // });
-
-    // console.log("data", data);
-    // getAllData();
     setPost("");
   }
 
   async function onDeletePost(id: string) {
-    // const { data } = await client.models.Post.delete({
-    //   id,
-    // });
     deleteMutation.mutate(id);
-    // console.log("data", data);
   }
 
   return (
@@ -135,6 +132,11 @@ function App() {
         <Button type="submit" variation="primary">
           Add Title
         </Button>
+        {mutation.isPaused || deleteMutation.isPaused ? (
+          <View color="red" textAlign="center">
+            You are offline! Data will be sent when online.
+          </View>
+        ) : null}
       </View>
       {query.data?.map((post) => (
         <Post key={post.id} post={post} onDelete={onDeletePost} />
--- src/main.tsx
+++ src/main.tsx
@@ -8,33 +8,61 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+// import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { generateClient } from "aws-amplify/api";
+import { Schema } from "../amplify/data/resource.ts";
+
+// const persister = createSyncStoragePersister({
+//   storage: window.localStorage,
+// });
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      staleTime: 2000,
+      retry: 0,
     },
   },
 });
+
+const client = generateClient<Schema>();
 
 const asyncStoragePersister = createAsyncStoragePersister({
   storage: window.localStorage,
 });
 // we need a default mutation function so that paused mutations can resume after a page reload
-// queryClient.setMutationDefaults(['titles'], {
-//   mutationFn: async ({ title }) => {
-//     // to avoid clashes with our optimistic update when an offline mutation continues
+queryClient.setMutationDefaults(["addTitles"], {
+  mutationFn: async (title) => {
+    // to avoid clashes with our optimistic update when an offline mutation continues
 
-//     await queryClient.cancelQueries({ queryKey: ["titles"] });
-//     return api.updateMovie(id, comment)
-//   },
-// })
+    console.log("title", title);
+    await queryClient.cancelQueries({ queryKey: ["titles"] });
+    const { data } = await client.models.Post.create({
+      title,
+    });
+    return Promise.resolve(data);
+  },
+});
+
+queryClient.setMutationDefaults(["deleteTitles"], {
+  mutationFn: async (id) => {
+    // to avoid clashes with our optimistic update when an offline mutation continues
+
+    await queryClient.cancelQueries({ queryKey: ["titles"] });
+    const { data } = await client.models.Post.delete({
+      id,
+    });
+    return Promise.resolve(data);
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* <QueryClientProvider client={queryClient}> */}
     <PersistQueryClientProvider
       client={queryClient}
+      // persistOptions={{ persister: asyncStoragePersister }}
       persistOptions={{ persister: asyncStoragePersister }}
       onSuccess={() => {
         // resume mutations after initial restore from localStorage was successful
